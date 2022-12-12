55211fc97dbcbf950684eff50d9109be0b93281c
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-12 17:06:08 -0500

    it begins!

--- iamput/iamconfig.py
+++ iamput/iamconfig.py
@@ -1,4 +1,17 @@
 # is a mod config library
+import pathlib
 
 class Config():
     pass
+
+class ConfigFile():
+    pass
+
+class ConfigFolder():
+    def __init__(
+                    self,
+                    path:pathlib.Path = pathlib.Path(""),
+                    backupPath:pathlib.Path = pathlib.Path("")
+                ):
+        self.path:pathlib.Path = path
+        self.backupPath:pathlib.Path = backupPath
