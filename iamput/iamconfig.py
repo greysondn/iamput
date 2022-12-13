dc0d05166bd01b9898a3ce9f8a5a9d6d1effac29
J. 'Dorian Greyson' L <greysondn@gmail.com>
2022-12-12 19:14:30 -0500

    implementation

--- iamput/iamconfig.py
+++ iamput/iamconfig.py
@@ -1,5 +1,7 @@
 # is a mod config library
+import os
 import pathlib
+import shutil
 
 class Config():
     pass
@@ -14,16 +16,46 @@ class ConfigFolder():
                     backupPath:pathlib.Path = pathlib.Path("")
                 ):
         self.path:pathlib.Path = path
-        self.backupPath:pathlib.Path = backupPath
         self.files:list["ConfigFile"]
+        
+    def delete(self):
+        """Deletes self. You were warned.
+        """
+        # destroy self
+        shutil.rmtree(self.path)
         
     def overwrite(self, other:"ConfigFolder"):
         """Overwrites all of other with self.
 
+        Basically, deletes, then writes into the folder.
+
         Args:
             other (ConfigFolder): folder to overwrite
         """
+        # before we break anything
         raise NotImplementedError()
+    
+        # we start by just deleting the other
+        other.delete()
+        
+        # build list of our files and folders
+        fDirs = []
+        fFiles = []
+        
+        for (root, dirs, files) in os.walk(self.path):
+            fDirs = fDirs + dirs
+            
+            for f in files:
+                fFiles.append(root + "\\" + f)
+        
+        # recreate directory tree
+        for dir in fDirs:
+            os.makedirs(
+                        str(other.path) + "\\" + dir,
+                        exist_ok= True
+                )
+        
+        # todo: WOOPS
     
     def mergeInto(self, other:"ConfigFolder"):
         """Merge self into other, overwriting files as needbe
